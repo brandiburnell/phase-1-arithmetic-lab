4930b4cdcb6de5b23ae82dd65c5b93e4de3bc498
Brandi Burnell <brandiburnell6@gmail.com>
2023-09-19 20:18:49 -0600

    passed all tests

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100);
+
+const num3 = 14;
+const num4 = 5;
+const mod = 14 % 5;
+
+const max = Math.max(1,2,3,20);
